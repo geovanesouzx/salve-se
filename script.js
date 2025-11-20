8ba57c32483c9080f126d562ad72a26550ba77b1
geovanesouzx <sjeje702@gmail.com>
2025-11-20 20:05:07 -0300

    Sua mensagem aqui

--- script.js
+++ script.js
@@ -69,10 +69,8 @@ const svgs = {
 // ============================================================
 
 function initCustomUI() {
-    // AQUI ESTÁ A MUDANÇA: Adicionei os :not(...) para ignorar os campos do modal
-    // Isso força os horários e dias a usarem o sistema nativo do celular/PC, que não trava.
-    document.querySelectorAll('select:not(.custom-init):not(#class-start):not(#class-end):not(#class-day)').forEach(createCustomSelect);
-
+    // VOLTAMOS AO ORIGINAL: Aplica o visual bonito em TODOS os selects
+    document.querySelectorAll('select:not(.custom-init)').forEach(createCustomSelect);
     document.querySelectorAll('input[type="date"]:not(.custom-init)').forEach(createCustomDatePicker);
 }
 
@@ -1270,7 +1268,10 @@ const timeSlots = [
     { start: "07:00", end: "08:00" }, { start: "08:00", end: "09:00" }, { start: "09:00", end: "10:00" },
     { start: "10:00", end: "11:00" }, { start: "11:00", end: "12:00" }, { start: "12:00", end: "13:00" },
     { start: "13:00", end: "14:00" }, { start: "14:00", end: "15:00" }, { start: "15:00", end: "16:00" },
-    { start: "16:00", end: "17:00" }, { start: "17:00", end: "18:00" }, { start: "18:30", end: "19:30" },
+    { start: "16:00", end: "17:00" }, { start: "17:00", end: "18:00" },
+    { start: "18:00", end: "19:00" },
+    { start: "18:30", end: "19:30" },
+    { start: "19:00", end: "20:00" },
     { start: "19:30", end: "20:30" }, { start: "20:30", end: "21:30" }, { start: "21:30", end: "22:30" }
 ];
 
